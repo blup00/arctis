d7581a4e1cfd8ff2b1a37026cc1b8828150b6fa3
Boris Osijek <borisasd@gmail.com>
2020-02-11 23:27:47 +0100

    Updates

---
+++ precache-manifest.51fe2b51e3bf370b060af663e4caa369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce0a85763faccff3d28dd0af9f37b8f",
+    "url": "/arctis/index.html"
+  },
+  {
+    "revision": "12f778bd80eba40f17d9",
+    "url": "/arctis/static/css/main.84da189a.chunk.css"
+  },
+  {
+    "revision": "d2b12b2ef86eebb50708",
+    "url": "/arctis/static/js/2.846fa82d.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/arctis/static/js/2.846fa82d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f778bd80eba40f17d9",
+    "url": "/arctis/static/js/main.61b9b7de.chunk.js"
+  },
+  {
+    "revision": "3b6c41601761f715165f",
+    "url": "/arctis/static/js/runtime-main.8eabdeeb.js"
+  }
+]);
